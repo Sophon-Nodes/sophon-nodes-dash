9e8cca92a57b33f7524b62a956e5ba7a45d4b776
md-xyz <mdevlin14@gmail.com>
2025-01-17 22:50:29 -0600

    update select styles and list view toggle

--- src/components/SelectComponent.vue
+++ src/components/SelectComponent.vue
@@ -2,17 +2,17 @@
 import { Icon } from '@iconify/vue'
 import { ref, watch, defineProps, defineEmits } from 'vue'
 import {
-  SelectContent,  
+  SelectRoot,
+  SelectTrigger,
+  SelectPortal,
+  SelectContent,
+  SelectViewport,
   SelectItem,
   SelectItemIndicator,
-  SelectItemText,  
-  SelectPortal,
-  SelectRoot,
+  SelectItemText,
+  SelectScrollUpButton,
   SelectScrollDownButton,
-  SelectScrollUpButton,  
-  SelectTrigger,
   SelectValue,
-  SelectViewport,
 } from 'radix-vue'
 
 const filter = ref();
@@ -27,47 +27,49 @@ watch(filter, async () => {
 
 <template>
   <SelectRoot v-model="filter" :defaultValue="props.itens.defaultVal">
-    <SelectTrigger
-      class="inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-grass11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-green9 outline-none"
+    <SelectTrigger 
+      class="inline-flex items-center justify-between rounded-lg border border-[#505255] border-opacity-30 bg-[#0A0C10] px-3 py-2 text-sm text-gray-400 shadow-card hover:bg-gray-900 gap-2"
       aria-label="Customise options"
     >
       <SelectValue placeholder="Select a option..." />
       <Icon
-        icon="radix-icons:chevron-down"
-        class="h-3.5 w-3.5"
+        icon="mingcute:down-line"
+        class="h-4 w-4"
       />
     </SelectTrigger>
 
     <SelectPortal>
-      <SelectContent
-        class="min-w-[160px] bg-white rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-[100]"
+      <SelectContent 
+        class="overflow-hidden rounded-lg border border-[#505255] border-opacity-30 bg-[#0A0C10] shadow-lg"
         position="popper"
         :side-offset="5"
       >
-        <SelectScrollUpButton class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
-          <Icon icon="radix-icons:chevron-up" />
-        </SelectScrollUpButton>
-
-        <SelectViewport class="p-[5px]">
-            <SelectItem
-              v-for="(option, index) in props.itens.obj"
-              :key="index"
-              class="text-[13px] leading-none text-grass11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-green9 data-[highlighted]:text-green1"
-              :value="option.value"
-            >
-              <SelectItemIndicator class="absolute left-0 w-[25px] inline-flex items-center justify-center">
-                <Icon icon="radix-icons:check" />
-              </SelectItemIndicator>
-              <SelectItemText>
-                {{ option.text }}
-              </SelectItemText>
-            </SelectItem>
+        <SelectViewport class="p-1">
+          <SelectScrollUpButton class="flex items-center justify-center h-[25px] bg-[#0A0C10] text-violet11 cursor-default">
+            <Icon icon="mingcute:up-line" />
+          </SelectScrollUpButton>
+          <SelectItem
+            v-for="(option, index) in props.itens.obj"
+            :key="index"
+            :value="option.value"
+            class="relative flex items-center px-6 py-2 text-sm text-gray-400 rounded-md outline-none hover:bg-gray-900 cursor-pointer data-[highlighted]:bg-gray-900 data-[highlighted]:text-white data-[state=checked]:text-white"
+          >
+            <SelectItemIndicator class="absolute left-1">
+              <Icon icon="mingcute:check-line" class="h-4 w-4" />
+            </SelectItemIndicator>
+            <SelectItemText>{{ option.text }}</SelectItemText>
+          </SelectItem>
+          <SelectScrollDownButton class="flex items-center justify-center h-[25px] bg-[#0A0C10] text-violet11 cursor-default">
+            <Icon icon="mingcute:down-line" />
+          </SelectScrollDownButton>
         </SelectViewport>
-
-        <SelectScrollDownButton class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
-          <Icon icon="radix-icons:chevron-down" />
-        </SelectScrollDownButton>
       </SelectContent>
     </SelectPortal>
   </SelectRoot>
 </template>
+
+<style>
+.shadow-card {
+  box-shadow: 0px 0px 0px 1px rgba(80, 82, 85, 0.30), 0px 1px 2px 0px rgba(0, 0, 0, 0.05), 0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px -1px rgba(0, 0, 0, 0.10);
+}
+</style>
